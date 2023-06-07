c6b93e910e9126489e597d12965f0beb30a1af0b
APuky <a.pukanic@gmail.com>
2023-06-07 12:54:10 +0200

    Responsivness, logout

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
@@ -24,6 +25,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
 
 
 function App() {
@@ -75,6 +77,11 @@ function App() {
     editTask !== id ? setEditTask(id) : setEditTask('')
   }
 
+  function handleLogoutClick(){
+    localStorage.removeItem("isLoggedIn")
+    navigate("/login")
+  }
+
 
   return (<>
     <Stack 
@@ -86,19 +93,21 @@ function App() {
     <Typography variant='h3'>
       Your TODO list
     </Typography>
+    <Button variant='outlined' endIcon={<LogoutIcon/>} onClick={handleLogoutClick}>LogOut</Button>
     <div className='Validation App'>
       {validation}
     </div>
-
+      <Container style={{ textAlign: "center" }}>
       <form onSubmit={handleSubmitClick} id="task-form">
-        <TextField required id="task" label="Task:" variant="outlined" value={todo} onChange={(e) => setTodo(e.target.value)} />
+          <TextField sx={{ m: 1 }}  required id="task" label="Task:" variant="outlined" value={todo} onChange={(e) => setTodo(e.target.value)} />
         <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="de" >
-          <DateTimePicker sx={{ ml: 1 }} required label="Due Date:" value={date} onChange={(e) => setDate(e)} />
+          <DateTimePicker sx={{ m: 1 }} required label="Due Date:" value={date} onChange={(e) => setDate(e)} />
         </LocalizationProvider>
       </form>
-      <Button size="small" variant="contained" type='submit' form="task-form">Update</Button>
+      <Button sx={{ m: 1 }} size="small" variant="contained" type='submit' form="task-form">Update</Button>
+    </Container>
 
-    <Container>
+      <Container>
       {todos.map(Todo =>
         <Card sx={{ minWidth:40, m:2 }} key={Todo.id}>
           <CardContent>
